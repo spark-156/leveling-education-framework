6644c921a6783cfe87bf4cbead6a7b2fd3acb51e
spark-156 <luca.bergman@student.hu.nl>
2021-07-02 00:40:31 +0200

    ✨ React router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
+import { TheRouter } from './TheRouter';
 import styles from  './styles/App.module.css';
 
 function App() {
   return (
     <div className={styles.container}>
-      <div>Hello world!</div>
+      <TheRouter />
     </div>
   );
 }
