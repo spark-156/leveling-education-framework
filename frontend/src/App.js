4fcf8d3dd94e4253bf98bdde4b6a371fc76a55f1
spark-156 <luca.bergman@student.hu.nl>
2021-07-02 00:08:49 +0200

    ✨ init react app

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import styles from  './styles/App.module.css';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <div>Hello world!</div>
+    </div>
+  );
+}
+
+export default App;
