fcb4a5aa9dc2e7bde8889990251ea9d4d3813445
spark-156 <lucabergman@hotmail.nl>
2021-07-07 17:14:40 +0200

    🥅 error handling for copy contents

--- frontend/src/components/CopyToClipboardButton.jsx
+++ frontend/src/components/CopyToClipboardButton.jsx
@@ -5,8 +5,12 @@ import copy from 'copy-to-clipboard';
 export function CopyToClipboardButton({ content }) {
 
     const setClipboard = (content) => {
-        copy(content)
-        Alert.success('Coppied contents to clipboard!')
+        try {
+            copy(content)
+            Alert.success('Coppied contents to clipboard!')
+        } catch {
+            Alert.error('Could not copy contents to clipboard. Please do it manually.')
+        }
     }
 
     return <IconButton icon={<Icon icon="copy" />} onClick={() => setClipboard(content)} />
