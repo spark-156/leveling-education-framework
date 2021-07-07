a73c91cebe196b32079b6a30e94c40a604fbf758
spark-156 <lucabergman@hotmail.nl>
2021-07-07 14:35:48 +0200

    ➕ added rsuite

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { TheRouter } from './TheRouter';
-import styles from  './styles/App.module.css';
+import styles from './styles/App.module.css';
+
+import 'rsuite/dist/styles/rsuite-default.css';
 
 function App() {
   return (
