67cfa968ef2ad2795e06b6a7435441b731fbe453
spark-156 <lucabergman@hotmail.nl>
2021-07-07 16:46:11 +0200

    ✨ Added copy button to hboi niveau

--- frontend/src/views/Hboi.jsx
+++ frontend/src/views/Hboi.jsx
@@ -15,50 +15,50 @@ export default function Home() {
   const [selectedHBOI, setSelectedHBOI] = useState(`${architectuurlaag} ${activiteit}`);
   const [selectedArchitectuurlaag, setSelectedArchitectuurlaag] = useState(architectuurlaag);
   const [selectedActiviteit, setSelectedActiviteit] = useState(activiteit);
-  
+
   useEffect(() => {
     setSelectedArchitectuurlaag(architectuurlaag);
     setSelectedActiviteit(activiteit);
     setSelectedHBOI(`${architectuurlaag} ${activiteit}`)
   }, [architectuurlaag, activiteit])
-  
-  useEffect(() => {    
+
+  useEffect(() => {
     history.replace(`/hboi/${selectedArchitectuurlaag}/${selectedActiviteit}`);
   }, [selectedArchitectuurlaag, selectedActiviteit])
-  
-  
+
+
   return (
-      <>
+    <>
       <Grid>
         <Col xs={24} sm={24} md={12} lg={12}>
           <SelectOutOf
-            title="Architectuurlagen" 
-            options={architectuurlagen} 
-            id="architectuurlagen" 
+            title="Architectuurlagen"
+            options={architectuurlagen}
+            id="architectuurlagen"
             setSelectedFunction={setSelectedArchitectuurlaag}
             selected={architectuurlaag}
           />
         </Col>
         <Col xs={24} sm={24} md={12} lg={12}>
-          <SelectOutOf 
-            title="Activiteiten" 
-            options={activiteiten} 
-            id="activiteiten" 
+          <SelectOutOf
+            title="Activiteiten"
+            options={activiteiten}
+            id="activiteiten"
             setSelectedFunction={setSelectedActiviteit}
             selected={activiteit}
           />
         </Col>
       </Grid>
-        {hboi[selectedHBOI] ?
-          <Grid>
-            <Col xs={24} sm={24} md={6} lg={6}><Card title="Niveau 1" content={hboi[selectedHBOI][0]} /></Col>
-            <Col xs={24} sm={24} md={6} lg={6}><Card title="Niveau 2" content={hboi[selectedHBOI][1]} /></Col>
-            <Col xs={24} sm={24} md={6} lg={6}><Card title="Niveau 3" content={hboi[selectedHBOI][2]} /></Col>
-            <Col xs={24} sm={24} md={6} lg={6}><Card title="Niveau 4" content={hboi[selectedHBOI][3]} /></Col>
-          </Grid>
-        : 
-          null
-        }
+      {hboi[selectedHBOI] ?
+        <Grid>
+          <Col xs={24} sm={24} md={6} lg={6}><Card copy title="Niveau 1" content={hboi[selectedHBOI][0]} /></Col>
+          <Col xs={24} sm={24} md={6} lg={6}><Card copy title="Niveau 2" content={hboi[selectedHBOI][1]} /></Col>
+          <Col xs={24} sm={24} md={6} lg={6}><Card copy title="Niveau 3" content={hboi[selectedHBOI][2]} /></Col>
+          <Col xs={24} sm={24} md={6} lg={6}><Card copy title="Niveau 4" content={hboi[selectedHBOI][3]} /></Col>
+        </Grid>
+        :
+        null
+      }
     </>
   )
 }
