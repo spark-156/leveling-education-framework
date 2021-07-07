74f86f49a2e988d08d0c0260646875e75bad0afd
spark-156 <lucabergman@hotmail.nl>
2021-07-07 16:45:50 +0200

    ✨ added optional Copy button to Card component to copy contents

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,9 +1,29 @@
-import { Panel } from 'rsuite';
+import { Panel, Grid, Col, Row } from 'rsuite';
 
-export function Card({ title, content }) {
-    return (
-        <Panel style={{ margin: "5px 0px"}} header={title} bordered>
-          {content}
-        </Panel>
-      );
+import { CopyToClipboardButton } from './CopyToClipboardButton';
+
+export function Card({ title, content, copy = false }) {
+
+  let header = title;
+
+  if (copy) {
+    header = <Grid fluid>
+      <Row>
+        <Col xs={18}>{title}</Col>
+        <Col xs={6} style={{ textAlign: "right" }}><CopyToClipboardButton content={content} /></Col>
+      </Row>
+    </Grid>
+  }
+
+  return (
+    <Panel
+      style={{ margin: "5px 0px" }}
+      bordered
+      header={
+        header
+      }
+    >
+      {content}
+    </Panel>
+  );
 }
