21cb833537e296f8ce8096e1d4a3e736a50402b7
spark-156 <lucabergman@hotmail.nl>
2021-07-07 16:50:27 +0200

    ✨ Added notification for copying to clipboard

--- frontend/src/components/CopyToClipboardButton.jsx
+++ frontend/src/components/CopyToClipboardButton.jsx
@@ -1,11 +1,12 @@
-import { IconButton, Icon } from "rsuite"
+import { IconButton, Icon, Alert } from "rsuite"
 
 import copy from 'copy-to-clipboard';
 
 export function CopyToClipboardButton({ content }) {
 
     const setClipboard = (content) => {
-        copy(content, { debug: true })
+        copy(content)
+        Alert.success('Coppied contents to clipboard!')
     }
 
     return <IconButton icon={<Icon icon="copy" />} onClick={() => setClipboard(content)} />
