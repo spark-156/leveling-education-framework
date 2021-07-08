83927c6eebe92207e5dc5fdc7444873df18b684e
spark-156 <lucabergman@hotmail.nl>
2021-07-08 02:51:55 +0200

    💄 added navbar to loading component

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 import { Loader } from 'rsuite';
+import { NavbarHBOI } from "./Navbar";
+
 
 export default function Loading() {
   return (
-    <Loader size="lg" center content="loading" />
+    <>
+      <NavbarHBOI />
+      <Loader size="lg" center content="loading" />
+    </>
   );
 }
