35315e32c154d019bf7bd1b14356667fd3dcfbb0
spark-156 <lucabergman@hotmail.nl>
2021-07-08 03:25:00 +0200

    ✨ added tooltip to copy to clipboard button

--- frontend/src/components/CopyToClipboardButton.jsx
+++ frontend/src/components/CopyToClipboardButton.jsx
@@ -1,17 +1,27 @@
-import { IconButton, Icon, Alert } from "rsuite"
+import { IconButton, Icon, Alert, Tooltip, Whisper } from "rsuite"
 
 import copy from 'copy-to-clipboard';
 
 export function CopyToClipboardButton({ content }) {
 
-    const setClipboard = (content) => {
-        try {
-            copy(content)
-            Alert.success('Coppied contents to clipboard!')
-        } catch {
-            Alert.error('Could not copy contents to clipboard. Please do it manually.')
-        }
-    }
+  const tooltip = (
+    <Tooltip>
+      Copy contents to clipboard
+    </Tooltip>
+  );
 
-    return <IconButton icon={<Icon icon="copy" />} onClick={() => setClipboard(content)} />
+  const setClipboard = (content) => {
+    try {
+      copy(content)
+      Alert.success('Coppied contents to clipboard!')
+    } catch {
+      Alert.error('Could not copy contents to clipboard. Please do it manually.')
+    }
+  }
+
+  return (
+    <Whisper placement="top" trigger="hover" speaker={tooltip}>
+      <IconButton icon={<Icon icon="copy" />} onClick={() => setClipboard(content)} />
+    </Whisper>
+  )
 }
