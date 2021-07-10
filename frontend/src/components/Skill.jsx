97baec09b2cf2de126c1bb6c7e9c6cbaf23f5bfe
spark-156 <lucabergman@hotmail.nl>
2021-07-10 15:02:25 +0200

    ♻️ refactor skill to use ant design

--- frontend/src/components/Skill.jsx
+++ frontend/src/components/Skill.jsx
@@ -1,6 +1,5 @@
 
-import { Grid, Row, Col } from "rsuite"
-import { Divider } from 'antd';
+import { Divider, Row, Col } from 'antd';
 
 import { CopyCard } from "./Card";
 
@@ -8,17 +7,17 @@ import { CopyCard } from "./Card";
 export function Skill({ title, description, afzonderlijketaken }) {
   const keys = Object.keys(afzonderlijketaken)
 
-  return (<Grid>
+  return (
     <Row gutter={12}>
       <Divider orientation="left">{title}</Divider>
-      <Col xs={24} sm={24} md={6} lg={6}>
+      <Col xs={24} sm={24} md={6} lg={6} xl={6}>
         <CopyCard
           title="Description"
           children={description}
           copy={description}
         />
       </Col>
-      {keys.map(key => (<Col xs={24} sm={24} md={6} lg={6} key={key}>
+      {keys.map(key => (<Col xs={24} sm={24} md={6} lg={6} xl={6} key={key}>
         <CopyCard
           key={key}
           title={key}
@@ -26,6 +25,5 @@ export function Skill({ title, description, afzonderlijketaken }) {
           copy={afzonderlijketaken[key]}
         />
       </Col>))}
-    </Row>
-  </Grid>)
+    </Row>)
 }
