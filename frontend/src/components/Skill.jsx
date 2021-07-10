e8f2c813e236c2b3bce964e0980f1408a907a5a5
spark-156 <lucabergman@hotmail.nl>
2021-07-10 14:32:03 +0200

    💄 beheersingniveaus and professionalskills now look the same

--- frontend/src/components/Skill.jsx
+++ frontend/src/components/Skill.jsx
@@ -1,19 +1,30 @@
 
 import { Grid, Row, Col } from "rsuite"
+import { Divider } from 'antd';
+
+import { Card } from "./Card";
+
 
 export function Skill({ title, description, afzonderlijketaken }) {
   const keys = Object.keys(afzonderlijketaken)
 
   return (<Grid>
     <Row gutter={12}>
-      <h4>{title}</h4>
+      <Divider orientation="left">{title}</Divider>
       <Col xs={24} sm={24} md={6} lg={6}>
-        <h6>Description</h6>
-        <div>{description}</div>
+        <Card
+          title="Description"
+          children={description}
+          copy={description}
+        />
       </Col>
       {keys.map(key => (<Col xs={24} sm={24} md={6} lg={6} key={key}>
-        <h6>{key}</h6>
-        <div>{afzonderlijketaken[key]}</div>
+        <Card
+          key={key}
+          title={key}
+          children={afzonderlijketaken[key]}
+          copy={afzonderlijketaken[key]}
+        />
       </Col>))}
     </Row>
   </Grid>)
--- frontend/src/views/Professionalskills.jsx
+++ frontend/src/views/Professionalskills.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Grid, Row, Col, Divider } from "rsuite";
+import { Grid, Row, Col } from "rsuite";
 
 import { Skill } from "../components/Skill"
 import { NavbarHBOI } from "../components/Navbar"
@@ -15,7 +15,6 @@ export default function Professionalskills() {
         <Col xs={24} sm={24} md={24} lg={24}>
           <h2>Professional skills</h2>
           <div style={divStyle}>Om een ICT-opdracht, bestaande uit een of meer ICT-beroepstaken, succesvol te kunnen uitvoeren, zijn samen met de vakinhoudelijke expertise ookprofessional skills vereist. Integraal onderdeel van deze skills is dat een ICT-professional leert van elke opdracht en op deze manier een eigen profielvoor het toekomstige werk ontwikkelt. ICT-beroepstaken, professional skills en persoonlijke ontwikkeling zijn daarbij onlosmakelijk met elkaarverbonden. Om dit alles te operationaliseren en te verbinden met de ICT-beroepstaken zijn de professional skills geformuleerd in vieraandachtsgebieden: toekomstgericht organiseren, onderzoekend probleemoplossen, persoonlijk leiderschap en doelgericht interacteren. De vieraandachtsgebieden overlappen elkaar en vullen elkaar aan.In de gekozen opzet is gestreefd naar herkenbaarheid, werkbaarheid, en overdraagbaarheid, zodat zowel onderwijs als bedrijfsleven ermee uit devoeten kunnen. De opzet is gebaseerd op de Dublin descriptoren, de hbo standaard, de resultaten van de HBO-i werkgroep ‘De Andere Skills’ en degedeelde visie op onderzoekend vermogen en probleem oplossen die vanuit verschillende hogescholen is opgebouwd (Andriessen et al., 2014; Losse,2016; Turnhout et al., 2013).Bij de uitvoering van elke ICT-beroepstaak zijn professional skills nodig om de opdracht tot een goed einde te brengen. De aandachtsgebieden van deprofessional skills zijn gelijk voor alle opdrachten. Het gedrag dat als professional skill nodig is om de ICT beroepstaken uit te kunnen voeren wordtverderop beschreven. Het niveau van deze skills wordt bepaald door het beheersingsniveau vereist vanuit de opdracht zoals elders per architectuurlaagen activiteit voor vier niveaus beschreven. Dit biedt mogelijkheden om het niveau van de skills in de loop van de opleiding te laten groeien. Op niveau 1zal de opdrachtgever bijvoorbeeld aangeven dat er een presentatie gegeven moet worden en wat de criteria daarvoor zijn, terwijl de student op niveau3 zelf moet bepalen welke vormen van communicatie het best passen in de context en dit kunnen verantwoorden.Er is bovendien ruimte voor profilering op het vereiste niveau van de skills door in de opdracht bijvoorbeeld meer nadruk te leggen op de(organisatorische) complexiteit van de context of juist op de (technische) complexiteit van de (vak)inhoud.Wat onder de professional skills wordt verstaan, is nader ingevuld door elk van de vier aandachtsgebieden in drie deelgebieden uit te werken.</div>
-          <Divider />
         </Col>
       </Row>
       <Skill
@@ -27,7 +26,6 @@ export default function Professionalskills() {
           Managen: "Inventariseert deeltaken, plant en bewaakt tijd, geld, kwaliteit en ethiek van de uitvoering van de werkzaamheden, herkent kansen enrisico’s en zorgt voor een toekomstgerichte inbedding van de oplossing in de organisatie."
         }}
       />
-      <Divider />
       <Skill
         title="Onderzoekend probleemoplossen"
         description="ICT-opdrachten kritisch vanuit verschillende perspectieven beschouwen, problemen identificeren, vinden van een effectieve aanpak en komen totpassende oplossingen."
@@ -37,7 +35,6 @@ export default function Professionalskills() {
           Oplossen: "Het zowel methodisch als creatief problemen op kunnen lossen, het vinden van alternatieven en het kritisch de eigen en andermansredeneerketen kunnen doorlopen."
         }}
       />
-      <Divider />
       <Skill
         title="Persoonlijk leiderschap"
         description="Ondernemend zijn rond ICT-opdrachten en persoonlijke ontwikkeling, daarbij aandacht hebbend voor het eigen leervermogen en voor ogen houdendwat voor ICT-professional en/of welk type functies men ambieert."
@@ -47,7 +44,6 @@ export default function Professionalskills() {
           "Persoonlijke profilering": "Onderzoeken wat voor type professional men op termijn zou willen zijn, welk beroepenveld en type functies men ambieert en hoe zichdaarin van anderen in de branche te onderscheiden."
         }}
       />
-      <Divider />
       <Skill
         title="Doelgericht interacteren"
         description="Bepalen welke partners een rol spelen bij de ICT-opdracht, constructief met hen samenwerken en passend communiceren gericht op de gewensteimpact."
