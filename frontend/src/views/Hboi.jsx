acb8df6b29b47f9ddd7664633c441810faf57442
spark-156 <lucabergman@hotmail.nl>
2021-07-12 23:15:35 +0200

    ✨ hboi select architectuurlaag or activiteit only

--- frontend/src/views/Hboi.jsx
+++ frontend/src/views/Hboi.jsx
@@ -14,19 +14,33 @@ export default function Hboi() {
   const history = useHistory();
   const { architectuurlaag, activiteit } = useParams();
 
-  const [selectedHBOI, setSelectedHBOI] = useState(`${architectuurlaag} ${activiteit}`);
   const [selectedArchitectuurlaag, setSelectedArchitectuurlaag] = useState(architectuurlaag);
   const [selectedActiviteit, setSelectedActiviteit] = useState(activiteit);
+
+  let htmlHBOI = null;
 
   useEffect(() => {
     setSelectedArchitectuurlaag(architectuurlaag);
     setSelectedActiviteit(activiteit);
-    setSelectedHBOI(`${architectuurlaag} ${activiteit}`)
+
   }, [architectuurlaag, activiteit])
 
   useEffect(() => {
     history.replace(`/hboi/${selectedArchitectuurlaag}/${selectedActiviteit}`);
   }, [selectedArchitectuurlaag, selectedActiviteit])
+
+
+  if (!(selectedArchitectuurlaag == "undefined" || selectedActiviteit == "undefined")) { // both selected
+    htmlHBOI = <Row style={rowStyle} gutter={[16, { xs: 8, sm: 16, md: 16, lg: 16, xl: 20 }]}>
+      {hboi[`${selectedArchitectuurlaag} ${selectedActiviteit}`].map((hboiNiveau, index) => <Col key={hboiNiveau} span={6} xs={24} sm={12} md={12} lg={6} xl={6}><CopyCard copy={hboiNiveau} title={`Niveau ${index + 1}`}>{hboiNiveau}</CopyCard></Col>)}
+    </Row>
+  }
+  if (!(selectedArchitectuurlaag === "undefined") && selectedActiviteit === "undefined") {
+    console.log('architectuurlaag only selected')
+  }
+  if (selectedArchitectuurlaag === "undefined" && !(selectedActiviteit === "undefined")) {
+    console.log('activiteit only selected')
+  }
 
   return (
     <>
@@ -52,13 +66,7 @@ export default function Hboi() {
         </Col>
       </Row>
       <br />
-      {hboi[selectedHBOI] ?
-        <Row style={rowStyle} gutter={[16, { xs: 8, sm: 16, md: 16, lg: 16, xl: 20 }]}>
-          {hboi[selectedHBOI].map((hboiNiveau, index) => <Col key={hboiNiveau} span={6} xs={24} sm={12} md={12} lg={6} xl={6}><CopyCard copy={hboiNiveau} title={`Niveau ${index + 1}`}>{hboiNiveau}</CopyCard></Col>)}
-        </Row>
-        :
-        null
-      }
+      {htmlHBOI}
     </>
   )
 }
