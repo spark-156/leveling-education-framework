9c0609002a2ed40ba37466edc30492d8cb25b2b5
spark-156 <lucabergman@hotmail.nl>
2021-07-12 23:42:07 +0200

    ✨ select solely arch, act or both at the same time

--- frontend/src/views/Hboi.jsx
+++ frontend/src/views/Hboi.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams, useHistory } from 'react-router-dom'
-import { Row, Col } from 'antd'
+import { Row, Col, Divider } from 'antd'
 
 import { CopyCard } from '../components/Card'
 import { NavbarHBOI } from '../components/Navbar'
@@ -30,43 +30,63 @@ export default function Hboi() {
   }, [selectedArchitectuurlaag, selectedActiviteit])
 
 
-  if (!(selectedArchitectuurlaag == "undefined" || selectedActiviteit == "undefined")) { // both selected
-    htmlHBOI = <Row style={rowStyle} gutter={[16, { xs: 8, sm: 16, md: 16, lg: 16, xl: 20 }]}>
-      {hboi[`${selectedArchitectuurlaag} ${selectedActiviteit}`].map((hboiNiveau, index) => <Col key={hboiNiveau} span={6} xs={24} sm={12} md={12} lg={6} xl={6}><CopyCard copy={hboiNiveau} title={`Niveau ${index + 1}`}>{hboiNiveau}</CopyCard></Col>)}
-    </Row>
+  if (!(selectedArchitectuurlaag === "undefined" || selectedActiviteit === "undefined")) { // both selected
+    htmlHBOI = <>
+      <Divider orientation="left">{selectedArchitectuurlaag} {selectedActiviteit}</Divider>
+      <Row style={rowStyle} gutter={[16, { xs: 8, sm: 16, md: 16, lg: 16, xl: 20 }]}>
+        {hboi[`${selectedArchitectuurlaag} ${selectedActiviteit}`].map((hboiNiveau, index) => <Col key={hboiNiveau} span={6} xs={24} sm={12} md={12} lg={6} xl={6}>
+          <CopyCard copy={hboiNiveau} title={`Niveau ${index + 1}`}>{hboiNiveau}</CopyCard>
+        </Col>)}
+      </Row>
+    </>
   }
-  if (!(selectedArchitectuurlaag === "undefined") && selectedActiviteit === "undefined") {
-    console.log('architectuurlaag only selected')
+  if (!(selectedArchitectuurlaag === "undefined") && selectedActiviteit === "undefined") { // architectuurlaag only selected
+    htmlHBOI = selectArchitectuurlagen[selectedArchitectuurlaag].map((architectuurlaagValue, index) => <>
+      <Divider orientation="left">{selectedArchitectuurlaag} {activiteiten[index]}</Divider>
+      <Row style={rowStyle} gutter={[16, { xs: 8, sm: 16, md: 16, lg: 16, xl: 20 }]}>
+        {architectuurlaagValue.map((niveau, index) => (
+          <Col key={niveau} span={6} xs={24} sm={12} md={12} lg={6} xl={6}>
+            <CopyCard copy={niveau} title={`Niveau ${index + 1}`}>{niveau}</CopyCard>
+          </Col>
+        ))}
+      </Row>
+    </>)
   }
-  if (selectedArchitectuurlaag === "undefined" && !(selectedActiviteit === "undefined")) {
-    console.log('activiteit only selected')
+  if (selectedArchitectuurlaag === "undefined" && !(selectedActiviteit === "undefined")) { // activiteit only selected
+    htmlHBOI = selectActiviteiten[selectedActiviteit].map((activiteitValue, index) => <>
+      <Divider orientation="left">{architectuurlagen[index]} {selectedActiviteit}</Divider>
+      <Row style={rowStyle} gutter={[16, { xs: 8, sm: 16, md: 16, lg: 16, xl: 20 }]}>
+        {activiteitValue.map((niveau, index) => (
+          <Col key={niveau} span={6} xs={24} sm={12} md={12} lg={6} xl={6}>
+            <CopyCard copy={niveau} title={`Niveau ${index + 1}`}>{niveau}</CopyCard>
+          </Col>
+        ))}
+      </Row>
+    </>)
   }
 
-  return (
-    <>
-      <NavbarHBOI active={"hboi"} />
-      <Row style={rowStyle} gutter={[16, { xs: 8, sm: 16, md: 16, lg: 16 }]}>
-        <Col span={12} xs={24} sm={24} md={12} lg={12} xl={12}>
-          <SelectOutOf
-            title="Architectuurlagen"
-            options={architectuurlagen}
-            id="architectuurlagen"
-            setSelectedFunction={setSelectedArchitectuurlaag}
-            selected={architectuurlaag}
-          />
-        </Col>
-        <Col span={12} xs={24} sm={24} md={12} lg={12} xl={12}>
-          <SelectOutOf
-            title="Activiteiten"
-            options={activiteiten}
-            id="activiteiten"
-            setSelectedFunction={setSelectedActiviteit}
-            selected={activiteit}
-          />
-        </Col>
-      </Row>
-      <br />
-      {htmlHBOI}
-    </>
-  )
+  return (<>
+    <NavbarHBOI active={"hboi"} />
+    <Row style={rowStyle} gutter={[16, { xs: 8, sm: 16, md: 16, lg: 16 }]}>
+      <Col span={12} xs={24} sm={24} md={12} lg={12} xl={12}>
+        <SelectOutOf
+          title="Architectuurlagen"
+          options={architectuurlagen}
+          id="architectuurlagen"
+          setSelectedFunction={setSelectedArchitectuurlaag}
+          selected={architectuurlaag}
+        />
+      </Col>
+      <Col span={12} xs={24} sm={24} md={12} lg={12} xl={12}>
+        <SelectOutOf
+          title="Activiteiten"
+          options={activiteiten}
+          id="activiteiten"
+          setSelectedFunction={setSelectedActiviteit}
+          selected={activiteit}
+        />
+      </Col>
+    </Row>
+    {htmlHBOI}
+  </>)
 }
