55a53b758dc2d2a5521aa34f6337f8ffd7afc2bd
spark-156 <lucabergman@hotmail.nl>
2021-07-12 22:34:55 +0200

    ♻️ ✨ antdesign and deselect functionality

--- frontend/src/components/SelectOutOf.jsx
+++ frontend/src/components/SelectOutOf.jsx
@@ -1,21 +1,25 @@
 
-import { Button } from 'rsuite';
+import { Button } from 'antd';
 
 import { CopyCard } from './Card';
 
 export default function SelectOutOf({ title, options, setSelectedFunction, selected }) {
 
-  function setState(element) {
-    setSelectedFunction(element.target.value)
+  const setState = (value) => {
+    if (value === selected) {
+      return setSelectedFunction(undefined);
+    } else {
+      return setSelectedFunction(value);
+    }
   }
 
   return <CopyCard title={title}>{
     options.map(option => {
-      let appearance = "ghost"
+      let appearance = ""
       if (option === selected) {
-        appearance = "default"
+        appearance = "primary"
       }
-      return (<Button key={option} value={option} onClick={setState} color="blue" appearance={appearance} block >
+      return (<Button key={option} onClick={(e) => { setState(e.target.innerText) }} color="blue" type={appearance} block >
         {option}
       </Button>)
     })
