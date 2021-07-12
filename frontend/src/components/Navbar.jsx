a7a2ba0e57719baf54a87a1352c5c52c28102fcf
spark-156 <lucabergman@hotmail.nl>
2021-07-12 20:50:20 +0200

    💄 added padding to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,26 +3,28 @@ import { HomeOutlined } from '@ant-design/icons';
 
 export function NavbarHBOI({ active }) {
 
-  return <Menu theme="light" selectedKeys={[active]} mode="horizontal">
-    <Menu.Item key="home" icon={<HomeOutlined />}>
-      <a href="/" target="_self" rel="noopener noreferrer">
-        Home
-      </a>
-    </Menu.Item>
-    <Menu.Item key="hboi">
-      <a href="/hboi/undefined/undefined" target="_self" rel="noopener noreferrer">
-        HBO-I
-      </a>
-    </Menu.Item>
-    <Menu.Item key="beheersingsniveaus">
-      <a href="/beheersingsniveaus" target="_self" rel="noopener noreferrer">
-        Beheersingsniveaus
-      </a>
-    </Menu.Item>
-    <Menu.Item key="professionalskills">
-      <a href="/professionalskills" target="_self" rel="noopener noreferrer">
-        Professionalskills
-      </a>
-    </Menu.Item>
-  </Menu>
+  return <div style={{ padding: "0 0 10px 0" }}>
+    <Menu theme="light" selectedKeys={[active]} mode="horizontal">
+      <Menu.Item key="home" icon={<HomeOutlined />}>
+        <a href="/" target="_self" rel="noopener noreferrer">
+          Home
+        </a>
+      </Menu.Item>
+      <Menu.Item key="hboi">
+        <a href="/hboi/undefined/undefined" target="_self" rel="noopener noreferrer">
+          HBO-I
+        </a>
+      </Menu.Item>
+      <Menu.Item key="beheersingsniveaus">
+        <a href="/beheersingsniveaus" target="_self" rel="noopener noreferrer">
+          Beheersingsniveaus
+        </a>
+      </Menu.Item>
+      <Menu.Item key="professionalskills">
+        <a href="/professionalskills" target="_self" rel="noopener noreferrer">
+          Professionalskills
+        </a>
+      </Menu.Item>
+    </Menu>
+  </div>
 }
