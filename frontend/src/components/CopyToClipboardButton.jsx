6bdbff5d1791a0e0500b0c0f0aa077322ebc7206
spark-156 <lucabergman@hotmail.nl>
2021-07-12 21:04:36 +0200

    ♻️ refactor copy to clipboard button to ant design

--- frontend/src/components/CopyToClipboardButton.jsx
+++ frontend/src/components/CopyToClipboardButton.jsx
@@ -1,27 +1,19 @@
-import { IconButton, Icon, Alert, Tooltip, Whisper } from "rsuite"
-
+import { Button, Tooltip, message } from 'antd';
+import { CopyOutlined } from "@ant-design/icons";
 import copy from 'copy-to-clipboard';
 
 export function CopyToClipboardButton({ content }) {
 
-  const tooltip = (
-    <Tooltip>
-      Copy contents to clipboard
-    </Tooltip>
-  );
-
   const setClipboard = (content) => {
     try {
       copy(content)
-      Alert.success('Coppied contents to clipboard!')
+      message.success('Coppied contents to clipboard!', [1])
     } catch {
-      Alert.error('Could not copy contents to clipboard. Please do it manually.')
+      message.error('Could not copy contents to clipboard. Please do it manually.', [1])
     }
   }
 
-  return (
-    <Whisper placement="top" trigger="hover" speaker={tooltip}>
-      <IconButton icon={<Icon icon="copy" />} onClick={() => setClipboard(content)} />
-    </Whisper>
-  )
+  return <Tooltip title="Copy card contents to clipboard">
+    <Button icon={<CopyOutlined />} onClick={() => setClipboard(content)} />
+  </Tooltip>
 }
