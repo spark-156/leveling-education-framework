423372285811dda4aded198ceeb7f81b4db9e094
spark-156 <lucabergman@hotmail.nl>
2021-07-12 16:03:21 +0200

    ♻️ refactor home to antdesign

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,4 +1,4 @@
-import { Grid, Row, Col } from "rsuite";
+import { Row, Col } from "antd";
 
 import { NavbarHBOI } from "../components/Navbar";
 
@@ -8,14 +8,12 @@ export default function Home() {
   }
   return (<>
     <NavbarHBOI active={"home"} />
-    <Grid>
-      <Row>
-        <Col>
-          <br />
-          <p style={divStyle}>This page holds the carbon copy of this <a href="https://www.vereniginghogescholen.nl/system/profiles/documents/000/000/242/original/HBO-i-domeinbeschrijving_2018.pdf?1563200208">pdf</a>. The purpose of this website is to make it a lot easier for any user to quickly find what they are looking for in the document without having to scroll through 26 pages of mostly useless or redundant information. This website also has small improvement of life tweaks such as a copy to clipboard button and very fast and easy to use HBO-I beroepstaken picker. The links are also shareable so you can easily send the HBO-I you are talking about to a friend without issue.</p>
-          <p style={divStyle}>This site was written by Luca Bergman in React with the <a href="https://rsuitejs.com/">React Suite</a> component library. The source code can be found <a href="https://github.com/spark-156/hboi-domeinbeschrijving">here</a>.</p>
-        </Col>
-      </Row>
-    </Grid>
+    <Row>
+      <Col span={24}>
+        <br />
+        <p style={divStyle}>This page holds the carbon copy of this <a href="https://www.vereniginghogescholen.nl/system/profiles/documents/000/000/242/original/HBO-i-domeinbeschrijving_2018.pdf?1563200208">pdf</a>. The purpose of this website is to make it a lot easier for any user to quickly find what they are looking for in the document without having to scroll through 26 pages of mostly useless or redundant information. This website also has small improvement of life tweaks such as a copy to clipboard button and very fast and easy to use HBO-I beroepstaken picker. The links are also shareable so you can easily send the HBO-I you are talking about to a friend without issue.</p>
+        <p style={divStyle}>This site was written by Luca Bergman in React with the <a href="https://rsuitejs.com/">React Suite</a> component library. The source code can be found <a href="https://github.com/spark-156/hboi-domeinbeschrijving">here</a>.</p>
+      </Col>
+    </Row>
   </>)
 }
