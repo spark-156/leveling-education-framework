167d73fbc7d7aff6a2616a50a2b01b295f325649
spark-156 <lucabergman@hotmail.nl>
2022-04-01 14:43:25 +0200

    better page not found page

--- frontend/src/views/PageNotFound.jsx
+++ frontend/src/views/PageNotFound.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from "react-router-dom";
+import { Button, Space } from "antd";
+import styles from "../styles/PageNotFound.module.css";
 
 export default function PageNotFound() {
   const history = useHistory();
 
-  const clickHandler = () => {
-    history.goBack();
-  }
-
-  return <div >
-    <div >Could not find the page you were looking for</div>
-    <button onClick={clickHandler}>Click me to go back!</button>
-  </div>
+  return (
+    <div className={styles.container}>
+      <div>Helaas bestaat deze pagina niet.</div>
+      <Space>
+        <Link to="/">
+          <Button type="primary">
+            Klik hier om naar de thuispagina te gaan!
+          </Button>
+        </Link>
+        <Button type="secondary" onClick={() => history.goBack()}>
+          Klik hier om terug te gaan!
+        </Button>
+      </Space>
+    </div>
+  );
 }
