97c53d26fbfb7faa2d4b894228f266e83e6018aa
spark-156 <lucabergman@hotmail.nl>
2022-05-16 19:23:08 +0200

    vaardigheid data

---
+++ frontend/src/data/open-ict-vaardigheden.js
@@ -0,0 +1,186 @@
+const vaardigheden = {
+    "Juiste kennis ontwikkelen": {
+        "1": {
+            "title": "Student bepaalt welke kennis ontbreekt en stelt daarvoor learning stories/tasks met learning doelen op. Student toont aan welke bronnen zijn gebruikt en welke kennis is ontwikkeld. Student past deze kennis toe in producten/deliverables en draagt deze kennis over aan peers. ",
+            "info": "# INFO 11\n\n**bold text**\n\n- item 1\n- item 2\n\n`code`"
+        },
+        "2": {
+            "title": "Student kijkt naar gangbare en toepasselijke kennis en technieken in het beroepenveld, maakt het zich eigen en deelt deze kennis aan peers en experts.  Student laat zien om te kunnen gaan met tegenstrijdigheden in beschikbare informatie (bronnen).",
+            "info": "info 12"
+        },
+        "3": {
+            "title": "Student kijkt naar innovatieve kennis en technieken in het beroepenveld, maakt deze zich eigen en deelt deze kennis aan peers en experts.",
+            "info": "info 13"
+        },
+        "4": {
+            "title": "Student combineert verschillende kennis bronnen om nieuwe kennis voor het beroepenveld te ontwikkelen.",
+            "info": "info 14"
+        }
+    },
+    "Kwalitatief product maken": {
+        "1": {
+            "title": "Student stelt acceptatie- en kwaliteitscriteria op voor de producten/deliverables met bronnen en argumentatie. Student toont aan hoe deze criteria zijn toegepast op het product. Student laat zien dat bij het opstellen van de criteria rekening is gehouden met toegankelijkheid en gegevensbescherming. Student laat zien in staat te zijn om producten op HBO-i niveau 1 te kunnen opleveren.",
+            "info": "info 11"
+        },
+        "2": {
+            "title": "Student neemt de waarde van het product voor de directe stakeholders mee in het opstellen van de acceptatiecriteria.  Student laat zien dat bij het opstellen van criteria rekening gehouden is met testen, duurzaamheid, overdraagbaarheid, inclusiviteit en security. Student laat zien in staat te zijn om producten op HBO-i niveau 2 te kunnen opleveren.",
+            "info": "info 12"
+        },
+        "3": {
+            "title": "Student neemt de waarde van het product voor de indirecte stakeholders mee in het opstellen van de acceptatiecriteria. Student laat zien dat bij het opstellen van criteria rekening is gehouden met schaalbaarheid en onderhoudbaarheid van de oplossing en dat ethische aspecten van de oplossing. Student laat zien in staat te zijn om producten op HBO-i niveau 3 te kunnen opleveren. ",
+            "info": "info 13"
+        },
+        "4": {
+            "title": "Student laat zien dat bij het opstellen van criteria rekening is gehouden met wetenschappelijke standaarden en de gekozen strategie. Student laat zien in staat te zijn om producten op HBO-i niveau 4 te kunnen opleveren welke zijn onderbouwd vanuit huidige geaccepteerde wetenschappelijke inzichten.",
+            "info": "info 14"
+        }
+    },
+    "Overzicht creëren": {
+        "1": {
+            "title": "Student staat stil bij uitdagingen en laat zien nagedacht te hebben over de mogelijke aanpakken voor een oplossing. Student toont aan welke bronnen daarbij zijn gebruikt. ",
+            "info": "info 11"
+        },
+        "2": {
+            "title": "Student laat zien dat op voor het product cruciale uitdagingen door middel van research stories/tasks methodisch mogelijke aanpakken zijn gevonden. Hiervoor stelt de student research stories/tasks op met bijbehorende en toepasselijke onderzoeksmethode.",
+            "info": "info 12"
+        },
+        "3": {
+            "title": "Student houdt rekening met aspecten komend uit SDGs en ethiek bij het methodische komen tot een aanpak voor een oplossing. ",
+            "info": "info 13"
+        },
+        "4": {
+            "title": "Student houdt rekening met aspecten komende uit het beroepenveld, maatschappij en wetenschap bij het methodische komen tot een aanpak voor een oplossing.",
+            "info": "info 14"
+        }
+    },
+    "Kritisch oordelen": {
+        "1": {
+            "title": "Student stelt de criteria op om een keuze te kunnen maken uit de mogelijke aanpakken. Student vergelijkt de mogelijke aanpaekken aan de hand van de gestelde criteria.",
+            "info": "info 11"
+        },
+        "2": {
+            "title": "Student laat zien dat op voor het product cruciale uitdaging door middel van research stories/tasks methodisch en onderbouwd een keuze wordt gemaakt op basis van vooraf opgestelde criteria. Student is in staat om gangbare normen, praktijken en meningen ter discussie te stellen.",
+            "info": "info 12"
+        },
+        "3": {
+            "title": "Student laat zien dat bij het opstellen van de criteria waarop beslissingen zijn genomen rekening wordt gehouden met de bedrijfscontext, SDGs en ethische afwegingen. De student laat zien kritisch te zijn richting de opdracht, de opdrachtrichting en de oplossingsruimte waarbij er aandacht is voor de vraag achter vraag. Student is in staat om gangbare normen, praktijken en meningen uit te dagen.",
+            "info": "info 13"
+        },
+        "4": {
+            "title": "Student laat zien dat bij het opstellen van de criteria waarop beslissingen zijn genomen rekening wordt gehouden met het beroepenveld, maatschappij en wetenschap. Student is in staat om bij te dragen in het vormen van nieuwe gangbare normen, praktijken en meningen.",
+            "info": "info 14"
+        }
+    },
+    "Samenwerken": {
+        "1": {
+            "title": "Student draagt bij aan het gezamenlijk resultaat en aan een professionele werksfeer. Student bespreekt persoonlijke plannen en stemt deze af met het team. Student is op de hoogte van wat teamleden doen en zorgt er voor dat taken gelijkwaardig verdeeld zijn. Student maakt conflicten bespreekbaar, voert peer reviews uit en helpt peers. ",
+            "info": "info 11"
+        },
+        "2": {
+            "title": "Student zorgt voor professionele communicatie met de direct betrokkenen. Student stelt zich coöperatief en respectvol op. Student zorgt ervoor dat de betrokkenen op de hoogte zijn van de eigen werkzaamheden. ",
+            "info": "info 12"
+        },
+        "3": {
+            "title": "Student zorgt voor professionele communicatie met indirect betrokkenen. Student zorgt ervoor dat de belangen en waarden van alle betrokkenen meegenomen worden bij de uitvoering van de opdracht. Student helpt anderen zich ook verder te ontwikkelen. ",
+            "info": "info 13"
+        },
+        "4": {
+            "title": "Student is in staat om strategische samenwerkingen aan te gaan/op te zetten zodat relevante partijen op de passende wijze betrokken worden rekening houdend met hun specifieke belangen en waarden.",
+            "info": "info 14"
+        }
+    },
+    "Boodschap delen": {
+        "1": {
+            "title": "Student brengt boodschap over in de vorm van bijvoorbeeld documenten, presentaties en besprekingen. Student kiest passende opbouw/structuur om het doel te bereiken. Student reageert passend op feedback en vragen. ",
+            "info": "info 11"
+        },
+        "2": {
+            "title": "Student sluit aan bij kennis en context van de doelgroep, onder andere met woordkeuze, middelen en taalgebruik. Student luistert actief en vraagt door om de ander te begrijpen. Student geeft opbouwende feedback. ",
+            "info": "info 12"
+        },
+        "3": {
+            "title": "Student overtuigt betrokkenen om het draagvlak voor het doel te vergroten, met aandacht voor de context waarin de betrokkenen zich bevinden. Student maakt onderscheid tussen feiten en meningen. ",
+            "info": "info 13"
+        },
+        "4": {
+            "title": "Student gebruikt de meningen en feiten van anderen in de onderbouwing van de boodschap. Student is in in staat om een bredere boodschap uit te kunnen dragen die verder gaat dan de eigen ideeën waarbij de boodschap niet direct in lijn is met de eigen visie en denkbeelden.",
+            "info": "info 14"
+        }
+    },
+    "Plannen": {
+        "1": {
+            "title": "Student maakt een korte termijn planning voor eigen taken, zet deze in logische volgorde en houdt zich aan de planning. Student past planning aan op de verwachte capaciteit. Student communiceert en verantwoordt verstoringen in de planning aan team. Student bereidt besprekingen voor. ",
+            "info": "info 11"
+        },
+        "2": {
+            "title": "Student maakt middenlange termijn planning voor team en zet deze om in een eigen korte termijn planning met taken. Student past de planning aan op de verwachte capaciteit van het team.",
+            "info": "info 12"
+        },
+        "3": {
+            "title": "Student maakt een lange termijn planning rekening houdend met de capaciteit van het team en de wensen van de direct betrokkenen.",
+            "info": "info 13"
+        },
+        "4": {
+            "title": "Student vertaalt de gekozen strategie en visie naar een lange termijn (mogelijk meerjaren) planning en past deze planning aan waar nodig. Student houdt bij het maken van deze planning rekening met de capaciteit van het team en de wensen direct en indirect betrokkenen.",
+            "info": "info 14"
+        }
+    },
+    "Aanpassen": {
+        "1": {
+            "title": "Student past zich aan op de gekozen werkwijze binnen team, bedrijf en school. Student past het eigen gedrag aan op feedback van anderen en reflectie van zichzelf. Student stelt zich meewerkend op bij lastige keuzes of onverwachte situaties en tegenslagen.",
+            "info": "info 11"
+        },
+        "2": {
+            "title": "Student is in staat om over eigen ideeën, aannames, vooroordelen en gemakken heen te stappen en/of ter discussie stellen om zo rekening te houden met duurzaamheid en diversiteit.  ",
+            "info": "info 12"
+        },
+        "3": {
+            "title": "Student is in staat om zich aan te passen aan veranderende strategieën. Student kan omgaan met concurrende doelen en stelt zich meewerkend op in de gekozen strategie.",
+            "info": "info 13"
+        },
+        "4": {
+            "title": "Student is in staat om te gaan met conflicterende belangen zowel voor zichzelf als met betrekking tot de strategie.",
+            "info": "info 14"
+        }
+    },
+    "Pro-actief handelen": {
+        "1": {
+            "title": "Student ziet kansen op de korte termijn en neemt actie voor eigen ontwikkeling en het (project)team. Student herkent problemen op korte termijn en neemt tijdig actie om deze te voorkomen of op te lossen.",
+            "info": "info 11"
+        },
+        "2": {
+            "title": "Student ziet kansen, neemt actie voor ontwikkeling en strategie van zichzelf, het team en de direct betrokkenen en koppelt de resultaten en gevolgen terug naar alle betrokkenen. Student herkent mogelijke problemen op de korten en langere termijn en neemt tijdig actie om deze te voorkomen of op te lossen.",
+            "info": "info 12"
+        },
+        "3": {
+            "title": "Student ziet kansen op de lange termijn en neemt tijdig actie om vernieuwende strategieën toe te kunnen passen in de uitvoering van de opdracht rekening houdend met alle betrokkenen. Student herkent mogelijke problemen voor zichzelf en de direct betrokkenen op de korten en langere termijn en neemt tijdig actie om deze te voorkomen of op te lossen.",
+            "info": "info 13"
+        },
+        "4": {
+            "title": "Student ziet kansen om vernieuwende strategieën te ontwikkelen ten behoeve van het beroepenveld en draagt deze uit. Student herkent mogelijke problemen en zorgt dat er tijdig actie wordt ondernomen om deze te voorkomen of op te lossen. De student gaat zodanig om met conflicterende belangen dat er geen problemen door ontstaan.",
+            "info": "info 14"
+        }
+    },
+    "Reflecteren": {
+        "1": {
+            "title": "Student haalt feedback op over uitgevoerd werk en functioneren. Student kijkt terug op wat goed gaat en wat beter kan. Student stelt ontwikkeldoelen op en werkt daar naar toe door middel van actiepunten te formuleren.  Student herkent hoe eigen gedrag zich verhoudt tot gangbare normen, praktijken en meningen.",
+            "info": "info 11"
+        },
+        "2": {
+            "title": "Student reflecteert cyclisch waarbij ontwikkeldoelen en actiepunten na verloop van tijd worden geëvalueerd en bijgesteld. Student neemt hierbij het functioneren van het team mee in deze reflectie.  Student herkent hoe perspectieven en gedrag van zichzelf en anderen impact hebben op het functioneren van het team. ",
+            "info": "info 12"
+        },
+        "3": {
+            "title": "Student zoekt bij het reflecteren naar de diepere onderliggende oorzaken van zowel positieve als negatieve aspecten van het eigen gedrag op het functioneren van zichzelf en anderen. Student neemt bij de reflectie mee hoe de student en het team bijdragen aan de uitvoering van de strategie.",
+            "info": "info 13"
+        },
+        "4": {
+            "title": "Student evalueert hoe de student en het team bij dragen aan het uitdragen van de strategie. Student kijkt wat in de omgeving impact heeft op het functioneren van zichzelf en het team, brengt dit in kaart en stelt daarbij het gedrag bij.",
+            "info": "info 14"
+        }
+    }
+}
+
+export {
+    vaardigheden
+}
