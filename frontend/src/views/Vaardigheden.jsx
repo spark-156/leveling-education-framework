fe574044549d8bbeb1887834f18aa42eaca41d78
spark-156 <lucabergman@hotmail.nl>
2022-05-18 14:35:59 +0200

    render and filter vaardigheden

--- frontend/src/views/Vaardigheden.jsx
+++ frontend/src/views/Vaardigheden.jsx
@@ -4,10 +4,64 @@ import useQueryString from "../lib/useQueryString";
 import SelectOutOf from "../components/SelectOutOf";
 import { rowStyle } from "../lib"
 import { vaardigheden } from '../data/open-ict-vaardigheden'
-import { Row, Col } from 'antd'
+import { Row, Col, Divider } from 'antd'
+import { CopyCard } from "../components/Card";
 
 export default function Vaardigheden() {
     const [vaardigheid, setSelectedVaardigheid] = useQueryString("vaardigheid");
+
+    let vaardighedenHtml = Object.keys(vaardigheden).map(key => (
+        <>
+            <Divider orientation="left">{key}</Divider>
+            <Row
+                style={rowStyle}
+                gutter={[16, { xs: 8, sm: 16, md: 16, lg: 16, xl: 20 }]}
+            >
+              {Object.keys(vaardigheden[key]).map(niveau => (
+                <Col
+                  key={vaardigheden[key][niveau]}
+                  span={6}
+                  xs={24}
+                  sm={12}
+                  md={12}
+                  lg={6}
+                  xl={6}
+                >
+                  <CopyCard copy={vaardigheden[key][niveau]['title']} title={`Niveau ${niveau}`}>
+                    {vaardigheden[key][niveau]['title']}
+                  </CopyCard>
+                </Col>
+              ))}
+            </Row>
+        </>
+    ))
+
+    if (vaardigheid) {
+      vaardighedenHtml = <>
+          <Divider orientation="left">{vaardigheid}</Divider>
+          <Row
+              style={rowStyle}
+              gutter={[16, { xs: 8, sm: 16, md: 16, lg: 16, xl: 20 }]}
+          >
+            {Object.keys(vaardigheden[vaardigheid]).map(niveau => (
+              <Col
+                key={vaardigheden[vaardigheid][niveau]}
+                span={6}
+                xs={24}
+                sm={12}
+                md={12}
+                lg={6}
+                xl={6}
+              >
+                <CopyCard copy={vaardigheden[vaardigheid][niveau]['title']} title={`Niveau ${niveau}`}>
+                  {vaardigheden[vaardigheid][niveau]['title']}
+                </CopyCard>
+              </Col>
+            ))}
+          </Row>
+      </>
+    }
+
 
     return <>
         <Row style={rowStyle} gutter={[16, { xs: 8, sm: 16, md: 16, lg: 16 }]}>
@@ -31,5 +85,6 @@ export default function Vaardigheden() {
                 />
             </Col>
         </Row>
+        {vaardighedenHtml}
     </>
 }
