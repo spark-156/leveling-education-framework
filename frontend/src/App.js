7a21ba460c1e895f1df45dfbc83467c07abf0d66
spark-156 <lucabergman@hotmail.nl>
2022-05-23 17:18:30 +0200

    new navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar } from "./components/Navbar";
 import { PageContainer } from "./components/PageContainer";
 import styles from "./styles/App.module.css";
@@ -6,44 +6,74 @@ import styles from "./styles/App.module.css";
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
 } from "react-router-dom";
-import { Footer } from "./components/Footer";
 import PageNotFound from "./views/PageNotFound";
 import ProfessionalSkills from "./views/Professionalskills";
 import Beheersingsniveaus from "./views/Beheersingsniveaus"
 import Hboi from "./views/Hboi";
 import RedirectHboi from "./views/RedirectHboi";
 import Vaardigheden from "./views/Vaardigheden";
+import { Layout } from "antd";
+import PageHeader from './components/Header'
+
+
+const { Sider, Content } = Layout
 
 function App() {
+  const [collapsed, setCollapsed] = useState(false)
+
+  function toggleCollapsed () {
+    setCollapsed(value => !value)
+  }
+
+
   return <Router>
-      <div className={styles.pageContainer}>
-        <Navbar />
-        <PageContainer className={styles.contentWrap}>
-          <Switch>
-            <Route exact path="/">
-              <Hboi />
-            </Route>
-            <Route path='/vaardigheden'>
-              <Vaardigheden />
-            </Route>
-            <Route path='/beheersingsniveaus'>
-              <Beheersingsniveaus />
-            </Route>
-            <Route path='/professionalskills'>
-              <ProfessionalSkills />
-            </Route>
-            <Route path='/hboi/:architectuurlaag/:activiteit'>
-              <RedirectHboi />
-            </Route>
-            <Route path='*'>
-              <PageNotFound />
-            </Route>
-          </Switch>
-        </PageContainer>
-        <Footer />
-      </div>
+    <Layout 
+      className={styles.pageContainer} 
+    >
+      <PageHeader
+        collapsed={collapsed}
+        toggleCollapsed={toggleCollapsed}
+      />
+      <Layout>
+        <Sider
+          collapsible
+          trigger={null}
+          collapsed={collapsed}
+          // style={{ overflow: 'auto', height: '', position: 'sticky', top: 0, left: 0, }}
+          breakpoint="lg"
+          collapsedWidth="0"
+          theme="light"
+        >
+          <Navbar toggleCollapsed={toggleCollapsed} />
+        </Sider>
+        <Content>
+          <PageContainer className={styles.contentWrap}>
+            <Switch>
+              <Route exact path="/">
+                <Hboi />
+              </Route>
+              <Route path='/vaardigheden'>
+                <Vaardigheden />
+              </Route>
+              <Route path='/beheersingsniveaus'>
+                <Beheersingsniveaus />
+              </Route>
+              <Route path='/professionalskills'>
+                <ProfessionalSkills />
+              </Route>
+              <Route path='/hboi/:architectuurlaag/:activiteit'>
+                <RedirectHboi />
+              </Route>
+              <Route path='*'>
+                <PageNotFound />
+              </Route>
+            </Switch>
+          </PageContainer>
+        </Content>
+      </Layout>
+    </Layout>
   </Router>
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,33 +1,46 @@
 import { Menu } from 'antd';
 import { Link } from 'react-router-dom';
-import { BookOutlined } from '@ant-design/icons';
+
 import { useLocation } from 'react-router-dom';
 
 export function Navbar() {
   const location = useLocation();
 
   return <div style={{ padding: "0 0 10px 0" }}>
-    <Menu theme="light" defaultSelectedKeys={['/']} selectedKeys={[location.pathname]} mode="horizontal">
-      <Menu.Item key="/" icon={<BookOutlined />}>
-        <Link to='/'>
-          Beroepstaken (HBO-i)
-        </Link>
-      </Menu.Item>
-      <Menu.Item key="/vaardigheden">
-        <Link to='/vaardigheden'>
-          Vaardigheden Open-ICT
-        </Link>
-      </Menu.Item>
-      <Menu.Item key="/beheersingsniveaus">
-        <Link to='/beheersingsniveaus'>
-          Beheersingsniveaus (HBO-i)
-        </Link>
-      </Menu.Item>
-      <Menu.Item key="/professionalskills">
-        <Link to='/professionalskills'>
-          Professional Skills (HBO-i)
-        </Link>
-      </Menu.Item>
+    <Menu 
+      theme="light"
+      defaultSelectedKeys={['/']} 
+      selectedKeys={[location.pathname]} 
+      mode="inline"
+    >
+      <Menu.ItemGroup 
+        title="HBO-I"
+      >
+        <Menu.Item key="/">
+          <Link to='/'>
+            Beroepstaken
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="/beheersingsniveaus">
+          <Link to='/beheersingsniveaus'>
+            Beheersingsniveaus
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="/professionalskills">
+          <Link to='/professionalskills'>
+            Professional Skills
+          </Link>
+        </Menu.Item>
+      </Menu.ItemGroup>
+      <Menu.ItemGroup 
+        title="Open-ICT"
+      >
+        <Menu.Item key="/vaardigheden">
+          <Link to='/vaardigheden'>
+            Vaardigheden
+          </Link>
+        </Menu.Item>
+      </Menu.ItemGroup>
     </Menu>
   </div>
 }
