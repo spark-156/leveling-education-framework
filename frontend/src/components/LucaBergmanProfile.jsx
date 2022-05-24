7dffae74387bef9a76af0b2c94d3af431d1c6269
spark-156 <lucabergman@hotmail.nl>
2022-05-24 16:44:10 +0200

    update Luca bergman profile

--- frontend/src/components/LucaBergmanProfile.jsx
+++ frontend/src/components/LucaBergmanProfile.jsx
@@ -3,5 +3,5 @@ import { Link } from './Link';
 
 
 export function LucaBergmanProfile () {
-    return <Link style={{ fontSize: "120%" }} href="https://www.lucabergman.nl"><Avatar src="/fotovanmij.jpeg" />Luca Bergman</Link>
+    return <Link style={{ fontSize: "1.05em", fontWeight: 630 }} href="https://www.lucabergman.nl"><Avatar src="/fotovanmij.jpeg" />Luca Bergman</Link>
 }
