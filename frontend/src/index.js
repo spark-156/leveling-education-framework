489c5df68643f16c52ed3ba2144658f4bada9e1e
spark-156 <lucabergman@hotmail.nl>
2022-05-24 15:50:04 +0200

    persisted store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist';
+
+import store from './store/configureStore'
+
 import './styles/index.css';
 import 'antd/dist/antd.css';
-import App from './App';
-import store from './store/configureStore'
-import { Provider } from 'react-redux'
+
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
