23aff7afbe35620ec950913dced5731a09060cfe
spark-156 <lucabergman@hotmail.nl>
2022-05-25 11:39:51 +0200

    this website has been made by

--- frontend/src/components/LucaBergmanProfile.jsx
+++ frontend/src/components/LucaBergmanProfile.jsx
@@ -1,7 +1,12 @@
 import { Avatar } from 'antd';
 import { Link } from './Link';
-
+import { useSelector } from 'react-redux';
+import { selectBreakpoint } from '../store/breakpointSlice';
 
 export function LucaBergmanProfile () {
-    return <Link style={{ fontSize: "1.05em", fontWeight: 630 }} href="https://www.lucabergman.nl"><Avatar src="/fotovanmij.jpeg" />Luca Bergman</Link>
+    const breakpoint = useSelector(selectBreakpoint)
+    return <>
+        {!breakpoint ? "This website has been made by: " : null}
+        <Link style={{ fontSize: "1.05em", fontWeight: 420, }} href="https://www.lucabergman.nl"><Avatar src="/fotovanmij.jpeg" size="large" alt='picteure of me'/>Luca Bergman</Link>
+    </>
 }
