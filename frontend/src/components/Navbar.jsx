e5e445967c3e982ff7bb586de90b49f6bfab426d
holt <75948709+SvenNijholt@users.noreply.github.com>
2022-11-05 15:23:15 +0100

    added darkmode and themeswitcher

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import { useSelector } from "react-redux";
 import { selectKey } from "../store/selectedKeySlice";
 import { selectBreakpoint } from "../store/breakpointSlice";
 import { LucaBergmanProfile } from "./LucaBergmanProfile";
+import { ThemeSwitch } from "./ThemeSwitch";
 
 export function Navbar () {
   const [visible, setVisible] = useState(false);
@@ -13,12 +14,13 @@ export function Navbar () {
   const breakpoint = useSelector(selectBreakpoint)
 
   return <PageHeader
-    style={{ background: "#fff", position: "sticky", top: 0, zIndex: 1, width: "100%" }}
+    style={{ position: "sticky", top: 0, zIndex: 1, width: "100%" }}
     onBack={() => setVisible(true)}
     title={title}
     backIcon={breakpoint ? <MenuOutlined /> : null}
-    extra={<LucaBergmanProfile />}
+    extra={<div style={{display: "inline"}}> <ThemeSwitch/><LucaBergmanProfile /></div>}
   >
+
     <Drawer
       title="Menu"
       placement="left"
---
+++ frontend/src/components/ThemeSwitch.jsx
@@ -0,0 +1,28 @@
+import { useThemeSwitcher } from "react-css-theme-switcher";
+import React from "react";
+import { HalfMoon, SunLight } from "iconoir-react";
+import Loading from "./Loading";
+
+export function ThemeSwitch(){
+  const { switcher, themes, currentTheme, status } = useThemeSwitcher();
+  const [, setIsDarkMode] = React.useState(false);
+
+  if (status === "loading") {
+    return <Loading />;
+  }
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(previous => {
+      switcher({ theme: previous ? themes.light : themes.dark });
+      return !previous;
+    });
+  };
+
+  return (
+    <div className="theme-switch" style={{display: "inline", position: "relative",verticalAlign:"middle", marginRight: 5,}}>
+      {
+        currentTheme === "light" ? <HalfMoon onClick={toggleDarkMode}/>  : <SunLight onClick={toggleDarkMode}/> 
+      }
+    </div>
+  );
+}
