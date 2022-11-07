62a5896008b333ec5da1eace79efd25374a2a1c8
spark-156 <57094525+spark-156@users.noreply.github.com>
2022-11-07 08:57:12 +0100

    Revert "Dark theme"

--- frontend/src/components/LucaBergmanProfile.jsx
+++ frontend/src/components/LucaBergmanProfile.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Avatar } from "antd";
 import { Link } from "./Link";
+import { useSelector } from "react-redux";
+import { selectBreakpoint } from "../store/breakpointSlice";
 
 export function LucaBergmanProfile () {
+  const breakpoint = useSelector(selectBreakpoint)
   return <>
-    <Link href="https://www.lucabergman.nl" style={{ textDecoration: "none" }}>
-      <Avatar src="/fotovanmij.jpeg" size="medium" alt='picteure of me'/> &nbsp;Luca Bergman
+    {!breakpoint ? "This website has been made by: " : null}
+    <Link href="https://www.lucabergman.nl">
+      <Avatar src="/fotovanmij.jpeg" size="large" alt='picteure of me'/>Luca Bergman
     </Link>
   </>
 }
