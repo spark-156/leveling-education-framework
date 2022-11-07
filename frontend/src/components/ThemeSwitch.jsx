8c5d497b676b52ec656017fb3c0785cc47687a3c
spark-156 <lucabergman@hotmail.nl>
2022-11-07 08:46:34 +0100

    update theme switcher with tooltip

--- frontend/src/components/ThemeSwitch.jsx
+++ frontend/src/components/ThemeSwitch.jsx
@@ -2,6 +2,7 @@ import { useThemeSwitcher } from "react-css-theme-switcher";
 import React from "react";
 import { HalfMoon, SunLight } from "iconoir-react";
 import Loading from "./Loading";
+import { Button, Tooltip } from "antd";
 
 export function ThemeSwitch(){
   const { switcher, themes, currentTheme, status } = useThemeSwitcher();
@@ -18,11 +19,9 @@ export function ThemeSwitch(){
     });
   };
 
-  return (
-    <div className="theme-switch" style={{display: "inline", position: "relative",verticalAlign:"middle", marginRight: 5, cursor:"pointer"}} >
-      {
-        currentTheme === "light" ? <HalfMoon onClick={toggleDarkMode}/>  : <SunLight onClick={toggleDarkMode}/> 
-      }
-    </div>
-  );
+  return (<Tooltip title={"Current theme is: " + currentTheme} >
+    <Button aria-label="switch theme">
+      { currentTheme === themes.light ? <HalfMoon onClick={toggleDarkMode}/>  : <SunLight onClick={toggleDarkMode}/> }
+    </Button>
+  </Tooltip>);
 }
