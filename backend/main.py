0dd9b51cc1f7dce0913f2372583fb56c02fe42ba
spark-156 <lucabergman@hotmail.nl>
2022-11-08 11:16:31 +0100

    format backend

--- backend/main.py
+++ backend/main.py
@@ -7,21 +7,24 @@ app = FastAPI()
 
 @app.get("/hboi")
 async def hboi(
-            architectuurlaag: str = Query(default=None, regex="Gebruikersinteractie|Organisatieprocessen|Infrastructuur|Software|Hardwareinterfacing"),
-            activiteit: str = Query(default=None, regex="Analyseren|Adviseren|Ontwerpen|Realiseren|Manage & Control"), 
-            niveau: str = Query(default=None, regex="1|2|3|4")
-        ):
+    architectuurlaag: str = Query(
+        default=None, regex="Gebruikersinteractie|Organisatieprocessen|Infrastructuur|Software|Hardwareinterfacing"),
+    activiteit: str = Query(
+        default=None, regex="Analyseren|Adviseren|Ontwerpen|Realiseren|Manage & Control"),
+    niveau: str = Query(default=None, regex="1|2|3|4")
+):
     hboi = json.load(open("json/hboi.json"))
 
     if architectuurlaag:
-        hboi = {key: value for key, value in hboi.items() if architectuurlaag in key}
+        hboi = {key: value for key,
+                value in hboi.items() if architectuurlaag in key}
 
     if activiteit:
         hboi = {key: value for key, value in hboi.items() if activiteit in key}
 
     if niveau:
         for key, value in hboi.items():
-            hboi[key] = { niveau: value[niveau] }
+            hboi[key] = {niveau: value[niveau]}
 
     return hboi
 
@@ -36,4 +39,3 @@ async def vaardigheden():
 async def vaardigheden(vaardigheid: str = Query(default=None, regex="Juiste kennis ontwikkelen|Kwalitatief product maken|Overzicht creëren|Kritisch oordelen|Samenwerken|Boodschap delen|Plannen|Flexibel opstellen|Pro-actief handelen|Reflecteren")):
     vaardigheden = json.load(open("json/vaardigheden.json"))
     return vaardigheden[vaardigheid]
-
