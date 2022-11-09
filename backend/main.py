9bd6d0cb9f18fb3ef95bc2b82eb12f9f966bf599
spark-156 <lucabergman@hotmail.nl>
2022-11-09 15:19:40 +0100

    rate limiting, cors and documentation

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,62 @@
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException, Query, Request, Response
+from fastapi.openapi.utils import get_openapi
+from fastapi.middleware.cors import CORSMiddleware
 import json
+from slowapi.errors import RateLimitExceeded
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+import uvicorn
 
-app = FastAPI()
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Leveling Education Framework",
+        version="1.0.0",
+        description="Leveling Education Framework public api for getting all HBO-I and Open-ICT Vaardigheden in json format.",
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
 
 
-@app.get("/hboi")
+limiter = Limiter(key_func=get_remote_address)
+app = FastAPI(responses={
+    200: {
+        "description": "Ok",
+        "content": {
+            "application/json": {
+
+            }
+        }
+    },
+    429: {
+        "description": "Rate limit exceeded",
+        "content": {
+            "application/json": {
+                "example": {"error": "Rate limit exceeded: 20 per 1 minute"}
+            }
+        }
+    }
+})
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.openapi = custom_openapi
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/hboi", tags=["HBO-I"])
+@limiter.limit(limit_value="20/minute")
 async def hboi(
+    request: Request,
+    response: Response,
     architectuurlaag: str = Query(
         default=None, regex="Gebruikersinteractie|Organisatieprocessen|Infrastructuur|Software|Hardwareinterfacing"),
     activiteit: str = Query(
@@ -28,16 +79,22 @@ async def hboi(
     return hboi
 
 
-@app.get("/vaardigheden")
-async def vaardigheden():
+@app.get("/vaardigheden", tags=["Open-ICT Vaardigheden"])
+@limiter.limit(limit_value="20/minute")
+async def vaardigheden(request: Request, response: Response):
     vaardigheden = json.load(open("json/vaardigheden.json"))
     return vaardigheden
 
 
-@app.get("/vaardigheden/{vaardigheid}")
-async def vaardigheden(vaardigheid: str = Query(default=None, regex="Juiste kennis ontwikkelen|Kwalitatief product maken|Overzicht creëren|Kritisch oordelen|Samenwerken|Boodschap delen|Plannen|Flexibel opstellen|Pro-actief handelen|Reflecteren")):
+@app.get("/vaardigheden/{vaardigheid}", description="'Juiste kennis ontwikkelen',\n'Kwalitatief product maken',\n'Overzicht creëren',\n'Kritisch oordelen',\n'Samenwerken',\n'Boodschap delen',\n'Plannen',\n'Flexibel opstellen',\n'Pro-actief handelen',\n'Reflecteren'", tags=["Open-ICT Vaardigheden"])
+@limiter.limit(limit_value="20/minute")
+async def vaardigheden(request: Request, response: Response, vaardigheid: str = Query(default=None)):
     vaardigheden = json.load(open("json/vaardigheden.json"))
     if vaardigheid not in vaardigheden:
         raise HTTPException(
             status_code=404, detail="Vaardigheid: " + vaardigheid + " not found")
     return vaardigheden[vaardigheid]
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
