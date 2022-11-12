b46397647ff8de7bbf2fc59b13485aa0e31659d6
spark-156 <lucabergman@hotmail.nl>
2022-11-12 17:04:34 +0100

    init nextjs

---
+++ frontend-new/next.config.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  i18n: {
+    locales: ['en', 'nl'],
+    defaultLocale: 'nl',
+    localeDetection: false
+  },
+}
+
+module.exports = nextConfig
