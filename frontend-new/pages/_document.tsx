1aca63ec358ae140943bfcd5a6dd88df28dbee6e
spark-156 <lucabergman@hotmail.nl>
2023-01-17 11:46:47 +0100

    fix favicon not showing up

--- frontend-new/pages/_document.tsx
+++ frontend-new/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Main, Head, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/logo_dark.svg" />
+        <link rel="icon" type="image/png" href="/logo_dark.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
