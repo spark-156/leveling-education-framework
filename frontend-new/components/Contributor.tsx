919010b70454f936e15e66f457317e7ea3dc4042
spark-156 <lucabergman@hotmail.nl>
2023-01-21 03:33:11 +0100

    about page

---
+++ frontend-new/components/Contributor.tsx
@@ -0,0 +1,32 @@
+import { Avatar, Stack, Typography } from "@mui/material";
+
+export default function Contributor(props: {
+  src?: string;
+  outsideSrc?: string;
+  name: string;
+  description: string;
+}) {
+  return (
+    <a
+      style={{ width: "100%" }}
+      target="_blank"
+      rel="noopener noreferrer"
+      href={props.outsideSrc ? props.outsideSrc : undefined}
+    >
+      <Stack alignItems="center" direction="row" gap={2}>
+        <Avatar src={props.src ? props.src : undefined}>
+          {props.src ? null : props.name.slice(0, 2)}
+        </Avatar>
+        <Stack justifyContent="center" direction="column" gap={0}>
+          <Typography
+            variant="h5"
+            sx={{ textDecoration: props.outsideSrc ? "underline" : "" }}
+          >
+            {props.name}
+          </Typography>
+          <Typography variant="body2">{props.description}</Typography>
+        </Stack>
+      </Stack>
+    </a>
+  );
+}
--- frontend-new/pages/about.tsx
+++ frontend-new/pages/about.tsx
@@ -4,14 +4,26 @@ import {
   CardHeader,
   CardMedia,
   Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Stack,
   Typography,
 } from "@mui/material";
-import { useIntl } from "react-intl";
+import { FormattedMessage, useIntl } from "react-intl";
 import { useContext } from "react";
 import { SelectedThemeContext } from "../context/SelectedThemeContext";
-
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import WebIcon from "@mui/icons-material/Web";
+import SchoolIcon from "@mui/icons-material/School";
+import Contributor from "../components/Contributor";
 export default function About() {
   const { selectedTheme } = useContext(SelectedThemeContext);
+  const cardMediaHeight = "400";
 
   const intl = useIntl();
   return (
@@ -21,12 +33,64 @@ export default function About() {
           <CardHeader title={intl.formatMessage({ id: "DEVELOPERS" })} />
           <CardMedia
             component="img"
-            height="400"
+            height={cardMediaHeight}
             image="/fotovanmij.jpeg"
             alt="Luca Bergman"
           />
           <CardContent>
             <Typography variant="h5">Luca Bergman</Typography>
+            <Typography variant="body1">
+              <FormattedMessage id="ABOUT_LUCA_BERGMAN_DESCRIPTION" />
+            </Typography>
+            <List>
+              <ListItem disablePadding>
+                <a
+                  style={{ width: "100%" }}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://lucabergman.nl/"
+                >
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <AccountCircleIcon />
+                    </ListItemIcon>
+                    <ListItemText>
+                      <FormattedMessage id="ABOUT_PROFILE_WEBSITE_TEXT" />
+                    </ListItemText>
+                  </ListItemButton>
+                </a>
+              </ListItem>
+              <ListItem disablePadding>
+                <a
+                  style={{ width: "100%" }}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.linkedin.com/in/luca-bergman-30b28b203/"
+                >
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <LinkedInIcon />
+                    </ListItemIcon>
+                    <ListItemText>LinkedIn</ListItemText>
+                  </ListItemButton>
+                </a>
+              </ListItem>
+              <ListItem disablePadding>
+                <a
+                  style={{ width: "100%" }}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/spark-156"
+                >
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <GitHubIcon />
+                    </ListItemIcon>
+                    <ListItemText>GitHub</ListItemText>
+                  </ListItemButton>
+                </a>
+              </ListItem>
+            </List>
           </CardContent>
         </Card>
       </Grid>
@@ -36,27 +100,80 @@ export default function About() {
 
           <CardMedia
             component="img"
-            height="400"
-            image={selectedTheme === "light" ? "OI_Black.svg" : "OI_White.svg"}
+            height={cardMediaHeight}
+            image={
+              selectedTheme === "light" ? "/OI_Black.svg" : "/OI_White.svg"
+            }
             alt="Open-ICT logo"
           />
           <CardContent>
             <Typography variant="h5">Open-ICT</Typography>
+            <List>
+              <ListItem disablePadding>
+                <a
+                  style={{ width: "100%" }}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.hu.nl/voltijd-opleidingen/open-ict"
+                >
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <SchoolIcon />
+                    </ListItemIcon>
+                    <ListItemText>
+                      <FormattedMessage id="ABOUT_EDUCATION_LINK_TEXT" />
+                    </ListItemText>
+                  </ListItemButton>
+                </a>
+              </ListItem>
+              <ListItem disablePadding>
+                <a
+                  style={{ width: "100%" }}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://husite.nl/open-ict/"
+                >
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <WebIcon />
+                    </ListItemIcon>
+                    <ListItemText>
+                      <FormattedMessage id="ABOUT_EDUCATION_SITE_TEXT" />
+                    </ListItemText>
+                  </ListItemButton>
+                </a>
+              </ListItem>
+            </List>
           </CardContent>
         </Card>
       </Grid>
       <Grid item xs={12} sm={6}>
         <Card>
-          <CardHeader title={intl.formatMessage({ id: "LOGO_BY" })} />
+          <CardHeader title={intl.formatMessage({ id: "CONTRIBUTORS" })} />
 
           <CardMedia
             component="img"
-            height="400"
-            image="/kevindemeijer.jpeg"
-            alt="Kevin de Meijer"
+            height={cardMediaHeight}
+            image="/thankyou.gif"
+            alt="Thank you gif"
           />
           <CardContent>
-            <Typography variant="h5">Kevin de Meijer</Typography>
+            <Stack gap={2}>
+              <Contributor
+                src="/kevindemeijer.jpeg"
+                name="Kevin de Meijer"
+                outsideSrc="https://nl.linkedin.com/in/kevindemeijer"
+                description={intl.formatMessage({
+                  id: "THANK_YOU_KEVIN_DE_MEIJER_DESC",
+                })}
+              />
+              <Contributor
+                name="Gideon Swaak"
+                description={intl.formatMessage({
+                  id: "THANK_YOU_GIDEON_SWAAK_DESC",
+                })}
+              />
+            </Stack>
           </CardContent>
         </Card>
       </Grid>
