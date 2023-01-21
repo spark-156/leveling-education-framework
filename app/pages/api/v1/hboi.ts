0b3e90095be8a18c02f0efaa4b8e8b8521fbc3d9
spark-156 <lucabergman@hotmail.nl>
2023-01-21 13:52:38 +0100

    delete old unused frontend and backend

--- app/pages/api/v1/hboi.ts
+++ app/pages/api/v1/hboi.ts
@@ -20,27 +20,36 @@ export default async function handler(
     architectuurlaag &&
     !Array.isArray(architectuurlaag) &&
     !architecture_layers.includes(architectuurlaag as Architectuurlaag)
-  ) {
+  )
     return res.status(400).json({
       error: `Architectuurlaag: ${architectuurlaag} not in: ${architecture_layers}`,
     });
-  }
+  else if (Array.isArray(architectuurlaag))
+    return res.status(400).json({
+      error: `Architectuurlaag: ${architectuurlaag} not allowed to be a list`,
+    });
 
   if (
     activiteit &&
     !Array.isArray(activiteit) &&
     !activities.includes(activiteit as Activiteit)
-  ) {
+  )
     return res.status(400).json({
       error: `Activiteit: ${activiteit} not in: ${activities}`,
     });
-  }
+  else if (Array.isArray(activiteit))
+    return res.status(400).json({
+      error: `Activiteit: ${activiteit} not allowed to be a list`,
+    });
 
-  if (niveau && !Array.isArray(niveau) && niveaus.includes(niveau as Niveau)) {
+  if (niveau && !Array.isArray(niveau) && !niveaus.includes(niveau as Niveau))
     return res.status(400).json({
       error: `Niveau: ${niveau} not in: ${niveaus}`,
     });
-  }
+  else if (Array.isArray(niveau))
+    return res.status(400).json({
+      error: `Niveau: ${niveau} not allowed to be a list`,
+    });
 
   return res.status(200).json(
     filterBeroepstaken(await getBeroepstakenOrVaardigheden("hboi"), {
