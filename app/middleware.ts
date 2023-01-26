99a0cc371408519190b377e6173d558e86a355d3
spark-156 <lucabergman@hotmail.nl>
2023-01-26 14:14:03 +0100

    HOTFIX fix cors headers blocking scorion api

--- app/middleware.ts
+++ app/middleware.ts
@@ -14,6 +14,7 @@ export function middleware(request: NextRequest) {
     "Cache-Control",
     `public, max-age=${CACHE_CONTROL_MAX_AGE}`
   );
+  response.headers.set("Access-Control-Allow-Origin", "*");
 
   return response;
 }
