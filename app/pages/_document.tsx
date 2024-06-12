8fde0bb64810e3f053043e8afefd1e70de3225ea
kaasbroodju <morriswaaijer8@gmail.com>
2024-06-12 22:09:20 +0200

    Add PWA functionality

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -3,15 +3,16 @@ import { Html, Main, Head, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <link rel="shortcut icon" href="/logo_dark.svg" />
-        <link rel="apple-touch-icon" href="/logo_dark.svg" />
-        <link rel="icon" type="image/png" href="/logo_dark.svg" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+        <Head>
+            <link rel="shortcut icon" href="/logo_dark.svg"/>
+            <link rel="apple-touch-icon" href="/logo_dark.svg"/>
+            <link rel="icon" type="image/png" href="/logo_dark.svg"/>
+            <link rel="manifest" href="/manifest.json"/>
+        </Head>
+        <body>
+        <Main/>
+        <NextScript/>
+        </body>
     </Html>
   );
 }
