cb4024b87f1d6429b7a97347807f738dc46fd72f
kaasbroodju <morriswaaijer8@gmail.com>
2024-06-14 09:13:34 +0200

    Revert "Add PWA functionality"
    This reverts commit 8fde0bb64810e3f053043e8afefd1e70de3225ea.

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,3 @@
-const withPWA = require('next-pwa')({
-  dest: 'public'
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -18,4 +14,4 @@ const nextConfig = {
   output: 'standalone'
 }
 
-module.exports = withPWA(nextConfig)
+module.exports = nextConfig
